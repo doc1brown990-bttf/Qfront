a8fa0c1a66d5f7276151ba82c0a5cf6f6450c56c
doc1brown990-bttf <doc1brown990@gmail.com>
2025-09-05 02:02:03 +0200

    Removing port fr url on socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://qback-l4yd.onrender.com:3001"); // backend Node.js
+const socket = io("https://qback-l4yd.onrender.com"); // backend Node.js
 
 export default socket;
