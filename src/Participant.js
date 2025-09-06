1e8d8763a0d45811f69028a9795915535a7c293a
doc1brown990-bttf <doc1brown990@gmail.com>
2025-09-06 02:36:07 +0200

    Update Participant.js

--- src/Participant.js
+++ src/Participant.js
@@ -1,25 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import socket from './socket';
 
-export default function Participants({ roomId }) {
-    const [currentQuestion, setCurrentQuestion] = useState(null);
+import React from "react";
+import LiveOrganizer from "./LiveOrganizer";
 
-    useEffect(() => {
-        if (!roomId) return;
-
-        socket.emit('joinRoom', roomId); // join della stanza
-
-        socket.on('nextQuestion', (question) => {
-            setCurrentQuestion(question.text);
-        });
-
-        return () => socket.off('nextQuestion');
-    }, [roomId]);
-
-    return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-            <h2>Schermata Partecipanti</h2>
-            {currentQuestion ? <p>{currentQuestion}</p> : <p>In attesa della prossima domanda...</p>}
-        </div>
-    );
-            }
+export default function Participants() {
+  return <LiveOrganizer isOrganizer={false} />;
+}
